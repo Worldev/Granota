5f7e587b115a0de4446e587a86b00ce86c021a9b
NeoMahler <roc.humet@gmail.com>
2015-04-02 19:22:41 +0200

    Actualització
    Immunització de l'owner del bot + mòdul multilingüe -> si s'escriu .bomb en comptes de .bomba el missatge surt en anglès (els colors també)

--- Granota/willie/modules/bomba.py
+++ Granota/willie/modules/bomba.py
@@ -12,7 +12,9 @@ from re import search
 import sched
 import time
 
-colors = ['Red', 'Yellow', 'Blue', 'White', 'Black']
+allcolors = ['Red', 'Yellow', 'Blue', 'White', 'Black', 'Vermell', 'Groc', 'Blau', 'Blanc', 'Negre'] 
+colors_en = ['Red', 'Yellow', 'Blue', 'White', 'Black'] 
+colors = ['Vermell', 'Groc', 'Blau', 'Blanc', 'Negre']
 sch = sched.scheduler(time.time, time.sleep)
 fuse = 120  # seconds
 bombs = dict()
@@ -31,30 +33,47 @@ def start(bot, trigger):
     global bombs
     global sch
     target = trigger.group(2).split(' ')[0]
-    if target in bot.config.other_bots or target == bot.nick:
-        return
-    if target == trigger.owner:
-        return
+    if target == bot.nick or target == bot.config.owner:
+        if trigger.group(1) == 'bomb':
+            bot.say(u"Yes, sure!")
+            return
+        else:
+            bot.say(u"Si home!")
+            return
     if target in bombs:
-        bot.say(u'No puc posar una altre bomba a ' + target + '!')
-        return
-    message = 'Ei, ' + target + u'! Sembla que algú t\'ha posat una bomba... Tens  2 minuts i 5 cables: Red, Yellow, Blue, White i Black. Quin cable he de tallar? No pateixis, jo sé el que em faig! (respon amb ".talla color")'
-    bot.say(message)
-    color = choice(colors)
-    bot.msg(trigger.nick,
-               u"Ei, no li diguis a %s, però és el cable %s! "
-               u"Però xxxt! No li diguis a ningú!" % (target, color))
-    code = sch.enter(fuse, 1, explode, (bot, trigger))
-    bombs[target.lower()] = (color, code)
-    sch.run()
+        if trigger.group(1) == 'bomb':
+            bot.say(u"I can't put another bomb to " + target + "!")
+            return
+        else:
+            bot.say(u'No puc posar una altre bomba a ' + target + '!')
+            return
+    if trigger.group(1) == 'bomb':
+        message = 'Hey, ' + target + u'! Somebody has given you a bomb! You have  2 minutes and 5 wires: Red, Yellow, Blue, White and Black. What wire should I cut? Don\'t worry, I know what I\'m doing ! (answer with ".cutwire color")'
+        bot.say(message)
+        color = choice(colors_en)
+        bot.msg(trigger.nick,
+                   u"Hey, don't tell it to %s, but is the %s one! "
+                   u"But sshhh! Don't tell it to anybody!" % (target, color))
+        code = sch.enter(fuse, 1, explode_en, (bot, trigger))
+        bombs[target.lower()] = (color, code)
+        sch.run()
+    else:       
+        message = 'Ei, ' + target + u'! Sembla que algÃº t\'ha posat una bomba... Tens  2 minuts i 5 cables: Vermell, Groc, Blau, Blanc i Negre. Quin cable he de tallar? No pateixis, jo sÃ© el que em faig! (respon amb ".talla color")'
+        bot.say(message)
+        color = choice(colors)
+        bot.msg(trigger.nick,
+                   u"Ei, no li diguis a %s, perÃ² Ã©s el cable %s! "
+                   u"PerÃ² xxxt! No li diguis a ningÃº!" % (target, color))
+        code = sch.enter(fuse, 1, explode, (bot, trigger))
+        bombs[target.lower()] = (color, code)
+        sch.run()
 
-
-@commands('cutwire','talla')
+@commands('cutwire', 'talla')
 def cutwire(bot, trigger):
     u"""
-    Talla el cable especificat quan algú et posa una bomba.
+    Talla el cable especificat quan algÃº et posa una bomba.
     """
-    global bombs, colors
+    global bombs, colors, colors_en, allcolors
     target = trigger.nick
     if target.lower() != bot.nick.lower() and target.lower() not in bombs:
         return
@@ -62,24 +81,43 @@ def cutwire(bot, trigger):
     wirecut = trigger.group(2).rstrip(' ')
     if wirecut.lower() in ('all', 'all!', 'tots', 'tot', 'tot!', 'tots!', 'todo', 'todos'):
         sch.cancel(code)  # defuse timer, execute premature detonation
-        kmsg = (u'KICK %s %s : Tallant TOTS els cables! *boom!!!* (Hauries d\'haver tallat el cable %s.)'
-                % (trigger.sender, target, color))
+        if trigger.group(1) == 'cutwire':
+            kmsg = (u'KICK %s %s:Cutting ALL wires! *boom!!!* (You should cut the %s one.)'
+                    % (trigger.sender, target, color))
+        else:
+            kmsg = (u'KICK %s %s:Tallant TOTS els cables! *boom!!!* (Hauries d\'haver tallat el cable %s.)'
+                    % (trigger.sender, target, color))
         bot.write([kmsg])
-    elif wirecut.capitalize() not in colors:
-        bot.say(u'No trobo aquest cable, ' + target + u'! Vols dir que no has d\'anar a l\'oculista? Aquest cable no existeix!')
+    elif wirecut.capitalize() not in allcolors:
+        if trigger.group(1) == 'cutwire':
+            bot.say(u'I don\'t see that wire, ' + target + u'! That wire doesn\'t exist!')
+        else:
+            bot.say(u'No trobo aquest cable, ' + target + u'! Vols dir que no has d\'anar a l\'oculista? Aquest cable no existeix!')
         bombs[target.lower()] = (color, code)  # Add the target back onto the bomb list,
     elif wirecut.capitalize() == color:
-        bot.say(u'Molt bé, ' + target + u'! Has aconseguit desactivar la bomba abans de que explotés! Bona feina!!')
+        if trigger.group(1) == 'cutwire':
+            bot.say(u'Well done, ' + target + u'! You have defused the bomb before exploding!!')
+        else:
+            bot.say(u'Molt bÃ©, ' + target + u'! Has aconseguit desactivar la bomba abans de que explotÃ©s! Bona feina!!')
         sch.cancel(code)  # defuse bomb
     else:
         sch.cancel(code)  # defuse timer, execute premature detonation
-        kmsg = 'KICK ' + trigger.sender + ' ' + target + \
-               u' : No! No, aquest és el dolent. Aii, t\'has matat a tu mateix... ho sento (Haguessis hagut de triar el cable ' + color + ')'
+        if trigger.group(1) == 'cutwire':
+            kmsg = 'KICK ' + trigger.sender + ' ' + target + \
+                   u' :No! No, that was the bad one. Ouch you have killed yourself... sorry (You should choose the ' + color + ' wire)'
+        else:
+            kmsg = 'KICK ' + trigger.sender + ' ' + target + \
+                   u' :No! No, aquest Ã©s el dolent. Aii, t\'has matat a tu mateix... ho sento (Haguessis hagut de triar el cable ' + color + ')'
         bot.write([kmsg])
-
 
 def explode(bot, trigger):
     target = trigger.group(2)
     kmsg = 'KICK ' + trigger.sender + ' ' + target + \
-           u' :Oh, vinga, ' + target + u'! Com a mínim ho haguessis pogut intentar! Ara estas mort.'
+           u' :Oh, vinga, ' + target + u'! Com a mÃ­nim ho haguessis pogut intentar! Ara estas mort.'
+    bot.write([kmsg])
+
+def explode_en(bot, trigger):
+    target = trigger.group(2)
+    kmsg = 'KICK ' + trigger.sender + ' ' + target + \
+           u' :Oh, come on, ' + target + u'! You are now dead. D:'
     bot.write([kmsg])

78683cc112ae823948cf16672c62cb3dec99dd18
NeoMahler <roc.humet@gmail.com>
2015-05-09 16:35:03 +0200

    Update config.py

--- Granota/willie/config.py
+++ Granota/willie/config.py
@@ -31,15 +31,6 @@ The configuration function, if used, must be declared with the signature
 ``configure(config)``. To add options, use ``interactive_add``, ``add_list``
 and ``add_option``.
 """
-"""
-Config - A config class and writing/updating utility for Willie
-Copyright 2012, Edward Powell, embolalia.net
-Copyright © 2012, Elad Alfassa <elad@fedoraproject.org>
-
-Licensed under the Eiffel Forum License 2.
-
-http://willie.dftba.net
-"""
 
 import db
 import os

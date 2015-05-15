1379ad352d501ddab458c36ea2821bdf5235916e
NeoMahler <roc.humet@gmail.com>
2015-05-15 19:15:16 +0200

    multilang

--- Granota/willie/modules/countdown.py
+++ Granota/willie/modules/countdown.py
@@ -1,33 +1,49 @@
-"""
-countdown.py - Willie Countdown Module
-Copyright 2011, Michael Yanovich, yanovich.net
-Licensed under the Eiffel Forum License 2.
-
-http://willie.dfbta.net
-"""
 from willie.module import commands, NOLIMIT
 import datetime
 
 
-@commands('countdown')
+@commands('countdown', 'comptenrere', 'cuentatras')
 def generic_countdown(bot, trigger):
     """
     .countdown <year> <month> <day> - displays a countdown to a given date.
     """
     text = trigger.group(2)
     if not text:
-        bot.say("Please use correct format: .countdown 2012 12 21")
+        if bot.config.lang == 'ca':
+            bot.reply(u"Utilitza el format correcte: .comptenrere 2018 12 19")
+        elif bot.config.lang == 'es':
+            bot.say(u"Utiliza el formato correcto: .cuentatras 2018 12 19")
+        else:
+            bot.say("Please use correct format: .countdown 2018 12 19")
         return NOLIMIT
     text = trigger.group(2).split()
     if text and (text[0].isdigit() and text[1].isdigit() and text[2].isdigit()
             and len(text) == 3):
         diff = (datetime.datetime(int(text[0]), int(text[1]), int(text[2]))
                 - datetime.datetime.today())
-        bot.say(str(diff.days) + " days, " + str(diff.seconds / 60 / 60)
-                   + " hours and "
-                   + str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60)
-                   + " minutes until "
-                   + text[0] + " " + text[1] + " " + text[2])
+        if bot.config.lang == 'ca':
+            bot.say(str(diff.days) + " dies, " + str(diff.seconds / 60 / 60)
+                       + " hores i "
+                       + str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60)
+                       + " minuts per "
+                       + text[0] + " " + text[1] + " " + text[2])
+        elif bot.config.lang == 'es':
+            bot.say(str(diff.days) + " dias, " + str(diff.seconds / 60 / 60)
+                       + " horas y "
+                       + str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60)
+                       + " minutos por "
+                       + text[0] + " " + text[1] + " " + text[2])
+        else:
+            bot.say(str(diff.days) + " days, " + str(diff.seconds / 60 / 60)
+                       + " hours and "
+                       + str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60)
+                       + " minutes for "
+                       + text[0] + " " + text[1] + " " + text[2])
     else:
-        bot.say("Please use correct format: .countdown 2012 12 21")
+        if bot.config.lang == 'ca':
+            bot.reply(u"Utilitza el format correcte: .comptenrere 2018 12 19")
+        elif bot.config.lang == 'es':
+            bot.say(u"Utiliza el formato correcto: .cuentatras 2018 12 19")
+        else:
+            bot.say("Please use correct format: .countdown 2018 12 19")
         return NOLIMIT

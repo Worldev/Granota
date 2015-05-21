e723e93fda48d59c5a72946b677f339c259475fe
NeoMahler <roc.humet@gmail.com>
2015-05-21 19:31:25 +0200

    multilang

--- Granota/willie/modules/isup.py
+++ Granota/willie/modules/isup.py
@@ -1,11 +1,3 @@
-"""
-isup.py - Simple website status check with isup.me
-Author: Edward Powell http://embolalia.net
-About: http://willie.dftba.net
-
-This allows users to check if a website is up through isup.me.
-"""
-
 from willie import web
 from willie.module import commands
 
@@ -15,21 +7,47 @@ def isup(bot, trigger):
     """isup.me website status checker"""
     site = trigger.group(2)
     if not site:
-        return bot.reply("What site do you want to check?")
+        if bot.config.lang == 'ca':
+            return bot.reply("Quina web vols que comprovi?")
+        elif bot.config.lang == 'es':
+            return bot.reply("Que web quieres que compruebe?")
+        else:
+            return bot.reply("What web do you want to check?")
 
     if site[:6] != 'http://' and site[:7] != 'https://':
         if '://' in site:
             protocol = site.split('://')[0] + '://'
-            return bot.reply("Try it again without the %s" % protocol)
+            if bot.config.lang == 'ca':
+                return bot.reply("Torna-ho a provar sense el %s" % protocol)
+            elif bot.config.lang == 'es':
+                return bot.reply("Vuelve a intentar sin el %s" % protocol)
+            else:
+                return bot.reply("Try it again without the %s" % protocol)
         else:
             site = 'http://' + site
     try:
         response = web.get(site)
     except Exception:
-        bot.say(site + ' looks down from here.')
+        if bot.config.lang == 'ca':
+            bot.say('Sembla que ' + site + ' no funciona o no existeix.')
+        elif bot.config.lang == 'es':
+            bot.say('Parece que ' + site + ' no funciona o no existe.')
+        else:
+            bot.say(site + ' looks down from here.')
         return
 
-    if response:
-        bot.say(site + ' looks fine to me.')
+    if response
+        if bot.config.lang == 'ca':
+            bot.say('No veig cap problema a ' + site)
+        elif bot.config.lang == 'es':
+            bot.say('No veo ningun problema en ' + site)
+        else:
+            bot.say(site + ' looks fine to me.')
     else:
-        bot.say(site + ' is down from here.')
+        if bot.config.lang == 'ca':
+            bot.say('Sembla que ' + site + ' no funciona o no existeix.')
+        elif bot.config.lang == 'es':
+            bot.say('Parece que ' + site + ' no funciona o no existe.')
+        else:
+            bot.say(site + ' looks down from here.')
+        return

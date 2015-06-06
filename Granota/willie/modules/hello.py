f8186c15e95d8e4607e7bc4ab75eac8fba1ef575
NeoMahler <roc.humet@gmail.com>
2015-06-06 19:45:09 +0200

    Update and rename saluda.py to hello.py

--- Granota/willie/modules/hello.py
+++ Granota/willie/modules/hello.py
@@ -5,19 +5,19 @@ def helloworld(bot, trigger):
     if bot.config.lang == 'ca':
         hi = "Hola "
         if trigger.group(2):
-            nick = trigger.group(2)
+            nick = trigger.group(2) + "!"
         else:
             nick = "a tots i a totes!"
     elif bot.config.lang == 'es':
         hi = "Hola "
         if trigger.group(2):
-            nick = trigger.group(2)
+            nick = trigger.group(2) + "!"
         else:
             nick = "a todos y a todas!"
     else:
         hi = "Hello "
         if trigger.group(2):
-            nick = trigger.group(2)
+            nick = trigger.group(2) + "!"
         else:
             nick = "everybody!"
     bot.say(hi + nick)

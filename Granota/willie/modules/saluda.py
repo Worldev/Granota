9523dd96cbbb9e84eb8d4515a0781f14cc89c8c1
NeoMahler <roc.humet@gmail.com>
2015-06-06 19:37:44 +0200

    multilang

--- Granota/willie/modules/saluda.py
+++ Granota/willie/modules/saluda.py
@@ -1,5 +1,23 @@
 import willie
 
-@willie.module.commands('saluda')
+@willie.module.commands('saluda', 'hello', 'hola')
 def helloworld(bot, trigger):
-    bot.say('Hola a tots i a totes!')
+    if bot.config.lang == 'ca':
+        hi = "Hola "
+        if trigger.group(2):
+            nick = trigger.group(2)
+        else:
+            nick = "a tots i a totes!"
+    elif bot.config.lang == 'es':
+        hi = "Hola "
+        if trigger.group(2):
+            nick = trigger.group(2)
+        else:
+            nick = "a todos y a todas!"
+    else:
+        hi = "Hello "
+        if trigger.group(2):
+            nick = trigger.group(2)
+        else:
+            nick = "everybody!"
+    return

fa0df2ebaefd63c8b57088e81d384e647c4eac62
NeoMahler <roc.humet@gmail.com>
2015-06-07 13:36:53 +0200

    Update ves.py

--- Granota/willie/modules/ves.py
+++ Granota/willie/modules/ves.py
@@ -3,14 +3,18 @@
 import willie
 import json
 from willie import web
-import time
 import re
 import urllib2
 
-@willie.module.commands('ves', u'curt')
+@willie.module.commands('ves', u'curt', 'shorturl', 'acorta')
 def ves(bot, trigger):
     if not trigger.group(2):
-        bot.say(u'Quina adreça vols escurçar?')
+        if bot.config.lang == 'ca':
+            bot.reply(u'Error de sintaxi. Escriu .curt <url>')
+        elif bot.config.lang == 'es':
+            bot.reply(u'Error de sintaxis. Escribe .acorta <url>')
+        else:
+            bot.reply(u'Syntax error. Use .shorturl <url>')
         return
     link = trigger.group(2)
     url = 'http://ves.cat/?url=%s&format=json' % link
@@ -18,4 +22,9 @@ def ves(bot, trigger):
         url = 'http://ves.cat/?url=http://%s&format=json' % link
     extracte = json.loads(web.get(url))
     linkcurt = extracte['link']
-    bot.say(u"L'adreça escurçada de %s és %s" % (link, linkcurt))
+    if bot.config.lang == 'ca':
+        bot.reply(u"L'adreça escurçada de %s és %s" % (link, linkcurt))
+    elif bot.config.lang == 'es':
+        bot.reply(u"El url acortado de %s és %s" % (link, linkcurt))
+    else:
+        bot.say(u"The short url of %s is %s" % (link, linkcurt))

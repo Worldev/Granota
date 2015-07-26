b0479aa04c45f70927271c435f97185adf030b80
NeoMahler <roc.humet@gmail.com>
2015-07-26 20:20:35 +0200

    Update setup.py

--- Granota/setup.py
+++ Granota/setup.py
@@ -18,14 +18,14 @@ def do_setup():
         setup(name='willie',
               version=__version__,
               description=u'An easy (and multilingual) IRC Bot',
-              author='Edward Powell',
-              author_email='powell.518@gmail.com',
-              url='http://willie.dftba.net/',
-              long_description="""Willie is a simple, lightweight, open source, easy-to-use IRC Utility bot, written in Python. It's designed to be easy to use, easy to run, and easy to make new features for. """,
+              author='CatBots team',
+              author_email='',
+              url='http://catbots.tk/',
+              long_description="""Granota is a simple, lightweight, open source, easy-to-use IRC Utility bot, written in Python. It's designed to be easy to use, easy to run, and easy to make new features for. """,
               packages=['willie', 'willie.modules'],
               scripts=[tmp_main_script],
               license='Eiffel Forum License, version 2',
-              platforms='Linux x86, x86-64',
+              platforms='Linux x86, x86-64 and Windows (Python required)',
               requires=['feedparser', 'pytz', 'lxml', 'praw', 'enchant', 'pygeoip']
              )
     finally:

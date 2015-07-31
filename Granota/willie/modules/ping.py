e0e636610e748d28af27d5bf3705ab202509ea69
Jesús "JeDa" Hernández <jedayoshi@gmail.com>
2015-07-31 19:05:59 -0430

    ¡Los admins también tenemos derechos!

--- Granota/willie/modules/ping.py
+++ Granota/willie/modules/ping.py
@@ -77,7 +77,7 @@ owner_ping_en = [u"I agree with you!",u"Our lord guides us very well...",
                                 
 @module.rule(r'(.*)?$nickname')
 def mention(bot, trigger):
-    if trigger.owner:
+    if trigger.owner or trigger.admin:
         dice = random.choice(['owner','nowner'])
         if dice == 'owner':
             if bot.config.lang == 'ca':

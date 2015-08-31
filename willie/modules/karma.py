9773ab15acb91154dbc4e99a18f1b82df8885b9a
NeoMahler <roc.humet@gmail.com>
2015-08-31 17:50:59 +0200

    added multilingual support

--- willie/modules/karma.py
+++ willie/modules/karma.py
@@ -34,15 +34,30 @@ def karma(bot, trigger):
     try: 
        karmas[user]
     except:
-       return bot.reply(oriuser + " never have been karmed!")
-    bot.reply(oriuser + " has " + str(karmas[user]) + " of karma.")
-
+       if bot.config.lang == 'ca':
+          return bot.reply(oriuser + " no ha rebut cap karma!")
+       elif bot.config.lang == 'es':
+          return bot.reply(oriuser + u" no ha recibido ningún karma!")
+       else:
+          return bot.reply(oriuser + " never have been karmed!")
+    if bot.config.lang == 'ca':
+       bot.reply(oriuser + u" t́e " + str(karmas[user]) + " punts de karma.")
+    elif bot.config.lang == 'es':
+       bot.reply(oriuser + u" tiene " + str(karmas[user]) + " puntos de karma.")
+    else:
+       bot.reply(oriuser + u" has " + str(karmas[user]) + " of karma.")
+       
 @willie.module.rule(r".*")
 def karmaman(bot, trigger):
     k = karmare.match(trigger.group(0))
     if k != None:
        if k.group(1).lower() == trigger.nick.lower():
-           return bot.notice(trigger.nick, "You can't karma yourself.")
+           if bot.config.lang == 'ca':
+              return bot.notice(trigger.nick, "No pots donar-te karma a tu mateix.")
+           elif bot.config.lang == 'es':
+              return bot.notice(trigger.nick, "No puedes darte karma a ti mismo.")
+           else:
+              return bot.notice(trigger.nick, "You can't karma yourself.")
 
        try:
           karmas[k.group(1).lower()]
@@ -50,10 +65,20 @@ def karmaman(bot, trigger):
           karmas[k.group(1).lower()] = 0
        if k.group(3) == "++":
           karmas[k.group(1).lower()] += 1
-          bot.notice(trigger.nick, "You have karmed up " + k.group(1) + ".")
+          if bot.config.lang == 'ca':
+             bot.notice(trigger.nick, "Has donat un punt de karma a " + k.group(1) + ".")
+          elif bot.config.lang == 'es':
+             bot.notice(trigger.nick, "Has dado un punto de karma a " + k.group(1) + ".")
+          else:
+             bot.notice(trigger.nick, "You have karmed up " + k.group(1) + ".")
        else:
           karmas[k.group(1).lower()] -= 1
-          bot.notice(trigger.nick, "You have karmed down " + k.group(1) + ".")
+          if bot.config.lang == 'ca':
+             bot.notice(trigger.nick, "Has tret un punt de karma a " + k.group(1) + ".")
+          elif bot.config.lang == 'es':
+             bot.notice(trigger.nick, "Has quitado un punto de karma a " + k.group(1) + ".")
+          else:
+             bot.notice(trigger.nick, "You have karmed down " + k.group(1) + ".")
        karmafilew.close()
        os.remove(homedir + "/karma.py")
        karmafile = open(homedir + "/karma.py", "w")

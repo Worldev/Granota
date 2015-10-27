4cf47434db76a7b58b0edab91f4f0ff1c2459dcb
NeoMahler <roc.humet@gmail.com>
2015-10-27 20:09:30 +0100

    Update announce.py

--- willie/modules/announce.py
+++ willie/modules/announce.py
@@ -18,13 +18,11 @@ def announce(bot, trigger):
         else:
             bot.reply(u"You are not my owner.")
         return
-    channels = []
-    for channel in bot.config.channels:
+    channels = bot.config.channels.split(',')
+    for channel in channels:
         if bot.config.lang == 'ca':
             bot.msg(channel, '[ANUNCI GLOBAL] %s' % trigger.group(2))
         elif bot.config.lang == 'es':
             bot.msg(channel, '[ANUNCIO GLOBAL] %s' % trigger.group(2))
         else:
             bot.msg(channel, '[GLOBAL ANNOUNCE] %s' % trigger.group(2))
-            channels.append(channel)
-    bot.say(str(bot.config.channels))

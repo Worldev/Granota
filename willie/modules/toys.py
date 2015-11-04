915ebff58ac854885457fdf5547f65631ec89d6f
NeoMahler <roc.humet@gmail.com>
2015-11-04 20:54:33 +0100

    rainbow toy

--- willie/modules/toys.py
+++ willie/modules/toys.py
@@ -20,3 +20,22 @@ def reversetext(bot, trigger):
         return
     text = trigger.group(2)
     bot.say(text[::-1])
+
+@commands('rainbow', 'arcoiris', 'colores', 'colors')
+def rainbow(bot, trigger):
+    """Speaks with rainbows."""
+    text = trigger.group(2)
+    if not text:
+        if bot.config.lang == 'ca':
+            bot.reply("Error de sintaxi. Escriu .colors <text>")
+        elif bot.config.lang == 'es':
+            bot.reply("Error de sintaxis. Escribe .arcoiris <texto>")
+        else:
+            bot.reply("Syntax error. Type .rainbow <text>")
+	colorlist = ["\x034","\x037","\x038","\x039","\x0311","\x0312","\x0313","\x036","\x034"]
+	rainbowed = ""
+	if raintype == "chan":
+    raintext = unicode(text, "utf8")
+	for letter in text:
+	    	rainbowed += random.choice(colorlist) + letter
+	bot.say(rainbowed.encode("utf8", "replace"))

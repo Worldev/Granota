29a397c3a2419d93b060e86a9a2ee4be210524b1
NeoMahler <roc.humet@gmail.com>
2015-11-04 20:42:13 +0100

    first toy

--- willie/modules/toys.py
+++ willie/modules/toys.py
@@ -1,1 +1,14 @@
-import willie
+from willie.module import commands
+
+@commands('reverse', 'reves')
+def reversetext(bot, trigger):
+    if not trigger.group(2):
+        if bot.config.lang == 'ca':
+            bot.reply("Error de sintaxi. Escriu .reves <text>")
+        elif bot.config.lang == 'es':
+            bot.reply("Error de sintaxis. Escribe .reves <texto>")
+        else:
+            bot.reply("Syntax error. Type .reverse <text>")
+        return
+    text = trigger.group(2)
+    bot.say(text[::-1])

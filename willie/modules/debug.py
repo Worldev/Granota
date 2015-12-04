08b5322fe6d2dbc30f4af587f390807502e9827f
NeoMahler <roc.humet@gmail.com>
2015-12-04 20:11:08 +0100

    Update debug.py

--- willie/modules/debug.py
+++ willie/modules/debug.py
@@ -3,9 +3,7 @@
 from willie.module import commands, example
 
 @commands('privs')
-@example('.privs', '.privs #channel')
 def privileges(bot, trigger):
-    """Print the privileges of a specific channel, or the entire array."""
     if trigger.group(2):
         try:
             bot.say(str(bot.privileges[trigger.group(2)]))
@@ -20,17 +18,13 @@ def privileges(bot, trigger):
         bot.say(str(bot.privileges))
 
 @commands('admins')
-@example('.admins')
 def admins(bot, trigger):
-    """Print the list of admins, including the owner."""
     owner = bot.config.core.owner
     admins = str(bot.config.core.get_list('admins'))
     bot.say("[Owner]"+owner+" [Admins]"+admins)
 
 @commands('debug_print')
-@example('.debug_print')
 def debug_print(bot, trigger):
-    """Calls version, admins and privileges prints in sequence."""
     try:
         willie.modules.version.version(bot, trigger)
     except Exception as e:
@@ -44,7 +38,5 @@ def debug_print(bot, trigger):
     privileges(bot, trigger)
 
 @commands('raiseException', 'causeProblems', 'giveError')
-@example('.raiseException')
 def cause_problems(bot, trigger):
-    """This deliberately causes willie to raise exceptional problems."""
     raise Exception("Problems were caused on command.")

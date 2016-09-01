67dcd417deb477579064eb23cfb6347901ea560e
NeoMahler <roc.humet@gmail.com>
2016-09-01 12:25:33 +0200

    Update logs.py

--- willie/modules/logs.py
+++ willie/modules/logs.py
@@ -2,3 +2,9 @@ import willie
 
 willie.module.rule(".*")
 willie.module.unblockable
+willie.module.rate(0)
+def log(bot, trigger):
+    """Sends all command calls to the logging channel"""
+    msg_command = "[LOG] %s (%s): %s"
+    if trigger.group(0).startswith(bot.config.prefix.replace('\', '')):
+        bot.msg(bot.config.logging_channel, msg_command % trigger.nick, trigger.channel, trigger.group(0))

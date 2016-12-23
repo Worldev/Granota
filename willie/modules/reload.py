3515332a876aa965b1fba44605cd8c993e7e61e3
NeoMahler <roc.humet@gmail.com>
2016-12-23 11:31:43 +0100

    Update reload.py

--- willie/modules/reload.py
+++ willie/modules/reload.py
@@ -121,6 +121,62 @@ def f_load(bot, trigger):
     bot.bind_commands()
 
     bot.reply(u'%r (version: %s)' % (module, modified))
+    
+@willie.module.nickname_commands("unload")
+@willie.module.priority("low")
+@willie.module.thread(False)
+def f_unload(bot, trigger):
+    if not trigger.admin:
+        if bot.config.lang == 'ca':
+            bot.reply(u'No tens permisos d\'administrador')
+        elif bot.config.lang == 'es':
+            bot.reply(u"No tienes permisos de administrador")
+        else:
+            bot.reply(u"You aren't an admin")
+        return
+
+    name = trigger.group(2)
+
+    if (not name) or (name == '*') or (name.upper() == 'ALL THE THINGS'):
+        bot.callables = None
+        bot.commands = None
+        bot.setup()
+        return bot.reply('done')
+
+    if not name in sys.modules:
+        if bot.config.lang == 'ca':
+            bot.reply(u"No hi ha cap mòdul anomenat " + name)
+        elif bot.config.lang == 'es':
+            bot.reply(u"No hay ningún module nombrado " + name)
+        else:
+            bot.reply(name + ": no such module!")
+        return
+
+    old_module = sys.modules[name]
+
+    old_callables = {}
+    for obj_name, obj in vars(old_module).iteritems():
+        if bot.is_callable(obj) or bot.is_shutdown(obj):
+            old_callables[obj_name] = obj
+
+    bot.unregister(old_callables)
+    # Also remove all references to willie callables from top level of the
+    # module, so that they will not get loaded again if reloading the
+    # module does not override them.
+    for obj_name in old_callables.keys():
+        delattr(old_module, obj_name)
+    
+    # Also delete the setup function
+    if hasattr(old_module, "setup"):
+        delattr(old_module, "setup")
+
+    # Thanks to moot for prodding me on this
+    path = old_module.__file__
+    if path.endswith('.pyc') or path.endswith('.pyo'):
+        path = path[:-1]
+    if not os.path.isfile(path):
+        return bot.reply('Found %s, but not the source file' % name)    
+    bot.say("Done?")
 
 if __name__ == '__main__':
     print __doc__.strip()

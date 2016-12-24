3837e5330837bfc535d37d94a64b73c97663131d
NeoMahler <roc.humet@gmail.com>
2016-12-24 19:18:38 +0100

    Update seen.py

--- willie/modules/seen.py
+++ willie/modules/seen.py
@@ -13,7 +13,12 @@ seen_dict = Ddict(dict)
 def seen(bot, trigger):
     """Reports when and where the user was last seen."""
     if not trigger.group(2):
-        bot.say(".seen <nick> - Reports when <nick> was last seen.")
+        if bot.config.lang == 'ca':
+            bot.say(u"He vist per última vegada a \x02%s\x02 ara mateix a \x02%s\x02, dient \x1D%s\x0F" % (trigger.nick, trigger.sender, trigger.group(0)))
+        elif bot.config.lang == 'es':
+            bot.say(u"He visto por última vez a \x02%s\x02 ahora mismo en \x02%s\x02, diciendo \x1D%s\x0F" % (trigger.nick, trigger.sender, trigger.group(0)))
+        else:
+            bot.say(u"I last saw \x02%s\x02 right now on \x02%s\x02, saying \x1D%s\x0F" % (trigger.nick, trigger.sender, trigger.group(0)))
         return
     nick = Nick(trigger.group(2).strip())
     if nick in seen_dict:
@@ -26,11 +31,20 @@ def seen(bot, trigger):
         saw = datetime.datetime.utcfromtimestamp(timestamp)
         timestamp = format_time(bot.db, bot.config, tz, trigger.nick,
                                 trigger.sender, saw)
-
-        msg = "I last saw %s at %s on %s, saying %s" % (nick, timestamp, channel, message)
+        if bot.config.lang == 'ca':
+            msg = u"He vist \x02%s\x02 per última vegada el dia \x02%s\x02 al canal \x02%s\x02, dient: \x1D%s\x0F" % (nick, timestamp, channel, message)
+        elif bot.config.lanf == 'es':
+            msg = u"He visto a \x02%s\x02 por última vez el día \x02%s\x02 en el canal \x02%s\x02, diciendo: \x1D%s\x0F" % (nick, timestamp, channel, message)
+        else:                                
+            msg = u"I last saw \x02%s\x02 at \x02%s\x02 on \x02%s\x02, saying \x1D%s\x0F" % (nick, timestamp, channel, message)
         bot.say(str(trigger.nick) + ': ' + msg)
     else:
-        bot.say("Sorry, I haven't seen %s around." % nick)
+        if bot.config.lang == 'ca':
+            bot.say(u"No recordo haver vist a \x02%s\x02" % nick)
+        elif bot.config.lang == 'es':
+            bot.say(u"No recuerdo haver visto a \x02%s\x02" % nick)
+        else:
+            bot.say("Sorry, I haven't seen \x02%s\x02 around." % nick)
 
 
 @rule('(.*)')

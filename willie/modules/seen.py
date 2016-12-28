c80378c02ede8fe98517dfc354ddb2206e022acb
NeoMahler <roc.humet@gmail.com>
2016-12-28 18:10:31 +0100

    Update seen.py

--- willie/modules/seen.py
+++ willie/modules/seen.py
@@ -58,6 +58,7 @@ def note(bot, trigger):
         try:
             with open(seen_dict, "r+") as f:
                 data = json.load(f)
+                data[name] = name
                 data[name]['timestamp'] = time.time()
                 data[name]['channel'] = trigger.sender
                 data[name]['message'] = trigger
@@ -67,6 +68,7 @@ def note(bot, trigger):
         except IOError:
             with open(seen_dict, "w+") as f:
                 data = json.load(f)
+                data[name] = name
                 data[name]['timestamp'] = time.time()
                 data[name]['channel'] = trigger.sender
                 data[name]['message'] = trigger

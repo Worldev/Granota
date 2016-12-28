bd8777e49e322db2491302f9970f40f8eaa565d4
NeoMahler <roc.humet@gmail.com>
2016-12-28 15:41:53 +0100

    Update seen.py

--- willie/modules/seen.py
+++ willie/modules/seen.py
@@ -20,13 +20,13 @@ def seen(bot, trigger):
         else:
             bot.say(u"I last saw \x02%s\x02 right now on \x02%s\x02, saying \x1D%s\x0F" % (trigger.nick, trigger.sender, trigger.group(0)))
         return
-    nick = trigger.nick
+    name = trigger.nick
     with open(seen_dict, "r") as f:
         data = json.load(f)
     if nick in data:
-        timestamp = data[nick]['timestamp']
-        channel = data[nick]['channel']
-        message = data[nick]['message']
+        timestamp = data[name]['timestamp']
+        channel = data[name]['channel']
+        message = data[name]['message']
 
         tz = get_timezone(bot.db, bot.config, None, trigger.nick,
                           trigger.sender)
@@ -34,19 +34,19 @@ def seen(bot, trigger):
         timestamp = format_time(bot.db, bot.config, tz, trigger.nick,
                                 trigger.sender, saw)
         if bot.config.lang == 'ca':
-            msg = u"He vist \x02%s\x02 per última vegada el dia \x02%s\x02 al canal \x02%s\x02, dient: \x1D%s\x0F" % (nick, timestamp, channel, message)
+            msg = u"He vist \x02%s\x02 per última vegada el dia \x02%s\x02 al canal \x02%s\x02, dient: \x1D%s\x0F" % (name, timestamp, channel, message)
         elif bot.config.lang == 'es':
-            msg = u"He visto a \x02%s\x02 por última vez el día \x02%s\x02 en el canal \x02%s\x02, diciendo: \x1D%s\x0F" % (nick, timestamp, channel, message)
+            msg = u"He visto a \x02%s\x02 por última vez el día \x02%s\x02 en el canal \x02%s\x02, diciendo: \x1D%s\x0F" % (name, timestamp, channel, message)
         else:                                
-            msg = u"I last saw \x02%s\x02 at \x02%s\x02 on \x02%s\x02, saying \x1D%s\x0F" % (nick, timestamp, channel, message)
+            msg = u"I last saw \x02%s\x02 at \x02%s\x02 on \x02%s\x02, saying \x1D%s\x0F" % (name, timestamp, channel, message)
         bot.say(str(trigger.nick) + ': ' + msg)
     else:
         if bot.config.lang == 'ca':
-            bot.say(u"No recordo haver vist a \x02%s\x02." % nick)
+            bot.say(u"No recordo haver vist a \x02%s\x02." % name)
         elif bot.config.lang == 'es':
-            bot.say(u"No recuerdo haber visto a \x02%s\x02." % nick)
+            bot.say(u"No recuerdo haber visto a \x02%s\x02." % name)
         else:
-            bot.say("Sorry, I haven't seen \x02%s\x02 around." % nick)
+            bot.say("Sorry, I haven't seen \x02%s\x02 around." % name)
 
 
 @rule('(.*)')
@@ -54,24 +54,24 @@ def seen(bot, trigger):
 @unblockable # Also tracks ignored users
 def note(bot, trigger):
     if trigger.sender.startswith("#"): # Only sees users that speak on public channels
-        nick = trigger.nick
+        name = trigger.nick
         try:
             with open(seen_dict, "r+") as f:
                 data = json.load(f)
-                data[nick] = nick
-                data[nick]['timestamp'] = time.time()
-                data[nick]['channel'] = trigger.sender
-                data[nick]['message'] = trigger
+                data[name] = name
+                data[name]['timestamp'] = time.time()
+                data[name]['channel'] = trigger.sender
+                data[name]['message'] = trigger
                 f.seek(0)  # rewind
                 f.write(json.dumps(data))
                 f.truncate()                    
         except IOError:
             with open(seen_dict, "w+") as f:
                 data = json.load(f)
-                data[nick] = nick
-                data[nick]['timestamp'] = time.time()
-                data[nick]['channel'] = trigger.sender
-                data[nick]['message'] = trigger
+                data[name] = name
+                data[name]['timestamp'] = time.time()
+                data[name]['channel'] = trigger.sender
+                data[name]['message'] = trigger
                 f.seek(0)  # rewind
                 f.write(json.dumps(data))
                 f.truncate()          

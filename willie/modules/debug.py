6de7f5b5eb7d7664db3189e7306b60a674697aec
NeoMahler <roc.humet@gmail.com>
2017-04-08 14:01:08 +0200

    Update debug.py

--- willie/modules/debug.py
+++ willie/modules/debug.py
@@ -101,9 +101,9 @@ def version(bot, trigger):
         bot.say(u"I'm \x02Granota %s\x02%s, on %s and using Python %s. %s" % (version, commitinfo, osver, pyver, latestmsg))
     
     update_msg = "I'm outdated! My current version is \x02%s\x02 but the latest stable version is \x02%s\x02! " + \
-    "Please download the last version here with some bugs fixed and nice features added: %s" % (version, latestver, latesturl)
+    "Please download the last version here with some bugs fixed and nice features added: %s"
     if latestver != version:
-        bot.msg(bot.config.owner, update_msg)
+        bot.msg(bot.config.owner, update_msg % (version, latestver, latesturl))
     
 @commands('debug_print')
 def debug_print(bot, trigger):
